bba775b5983ebf9ddbd19e2c1cb9baee3a3e7a73
KimtyShrestha <kimtyshrestha@gmail.com>
2026-01-05 18:11:13 +0545

    User Model

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const PORT: number = process.env.PORT
+  ? parseInt(process.env.PORT)
+  : 5050;
+export const MONGODB_URI: string =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/store_sync_backend";
+// Application level constants, with fallbacks
+// if .env variables are not set
+
+export const JWT_SECRET: string = process.env.JWT_SECRET || "store_sync_secret";
